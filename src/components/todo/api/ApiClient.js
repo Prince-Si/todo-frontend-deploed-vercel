8d56dfd393a253bf3f60fcd77e7222f9159516c6
Prince Singh <136824628+Prince-Si@users.noreply.github.com>
2026-01-07 16:26:25 +0530

    Update ApiClient.js

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 
 export const apiClient = axios.create(
     {
-        baseURL: 'http://129.154.252.168:8081'
+        baseURL: 'https://todoapi-oci.princesingh.co.in'
     }
 );
