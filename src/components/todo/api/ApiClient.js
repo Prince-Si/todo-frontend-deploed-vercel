e3a9d02b18a6e4c5205823655d98cc6b64680075
Prince Singh <singhprincelaxmi@gmail.com>
2026-01-08 20:00:37 +0530

    made some responsivness updates to the header

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 export const apiClient = axios.create(
     {
-        //baseURL: 'http://localhost:8081'
         baseURL: 'https://todoapi-oci.princesingh.co.in'
     }
 );
